a94575ad7a9650935655d2efab0248c6e33e963d
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-08 17:14:37 -0300

    Entendendo Operation system(os) para pegar dados da máquina do usuário

---
+++ reference/os_demo.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+// Plataform (Sistema operacional)
+console.log(os.platform());
+
+// CPU Arch (Bits)
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+// Home directory
+console.log(os.homedir());
