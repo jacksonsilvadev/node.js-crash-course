3e17d63393d33a812c3427435e273df50e3143f8
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-09 11:46:08 -0300

    Adicionado controle de rotas com o http

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello Node.js');
-    res.end();
+    if (req.url === '/')
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+    res.end('<h1>Home</h1>');
+    if (req.url === '/about')
+        res.end('<h1>About</h1>')
+    if (req.url === '/node')
+        res.end('<h1>I love Node.js</h1>');
 });
 
 // Process é um metódo do Node aonde faz com que consigamos pegar o estado atual global da minha aplicação (Node.js);
